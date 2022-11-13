de95a56b5b129940f5f9b8bd5159275381053c22
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-13 12:29:08 -0600

    Server JS boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server hosting at http://localhost:${POST}`);
+})
