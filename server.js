ec74c8d1e3ab5fc4b5e57ad766efef3c99fa1c2e
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-13 12:59:02 -0600

    Post requests data collection

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+//Import fs read/write function from helper js file
 
 const PORT = 3001;
 
 const app = express();
 
+// Imports everything in public so index.html and notes.html are styled
 app.use(express.static('public'));
-
+// Middleware to parse json post requsts
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Home page 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
-
+// Notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
-
+// Takes notes post request and makes a newNote object
 app.post('/notes', (req, res) => {
     console.log(`${req.method} request recieved.`)
-    let response;
+    let newNote;
+
     if (req.body && req.body.title) {
-        response = {
-            status: 'success',
-            data: req.body,
+        newNote = {
+            title: req.body.title,
+            text: req.body.text,
         };
-        res.json(`Recieved note for ${response.data.title}.`)
+        res.json(`Recieved note for ${newNote.title}.`)
     } else {
         res.json(`Note must at least contain a title.`)
     }
