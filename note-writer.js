4f4c59d846c6d927f53a9bcc31f5774b1b51c4ff
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-13 13:20:51 -0600

    DB read and parsed

--- note-writer.js
+++ note-writer.js
@@ -1,18 +1,39 @@
 const db = require('./db/db.json')
+const fs = require('fs');
 
 class NoteWriter {
     // Read db.json, add newNote, then write the file
-    readDB() {
-        console.log(db);
+    writeToDB(newNote) {
+        // let newNoteString = JSON.stringify(newNote);
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const parsedDB = JSON.parse(data);
+                parsedDB.push(newNote);
+            }
+        })
     }
 }
 
-console.log(db);
-let test = {
-    title: 'Test Push',
-    text: 'Silly fun test'
-}
-db.push(test);
-console.log(db);
+// console.log(db);
+// let test = {
+//     title: 'Test Push',
+//     text: 'Silly fun test'
+// }
+// let stringTest = JSON.stringify(test);
+// console.log(stringTest);
+
+// db.push(test);
+// console.log(db);
+
+// fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         const parsedDB = JSON.parse(data);
+//         console.log(parsedDB);
+//     }
+// })
 
 module.exports = NoteWriter;
