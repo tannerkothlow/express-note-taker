3b5d37e21d68608cfc19cdef21c89900fcced67a
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-15 10:13:34 -0600

    db updated for get calls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const NoteWriter = require('./helpers/note-writer.js');
-const db = require('./db/db.json');
+// const db = require('./db/db.json');
 const uuid = require('./helpers/uuid.js');
 //Import fs read/write function from helper js file
 
@@ -39,7 +39,11 @@ app.get('/notes/:id', (req, res) => {
     }
 })
 // Notes API
-app.get('/api/notes', (req, res) => res.json(db));
+app.get('/api/notes', (req, res) => {
+    // Redeclares db to update notes.
+    const db = require('./db/db.json');
+    res.json(db)
+});
 // Takes notes post request and makes a newNote object
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request recieved.`)
